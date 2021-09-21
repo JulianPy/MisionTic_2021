dbb3bc9e56c8f5399df96b0a1b48368971ec9b5b
JulianPy <jdvargasl@unal.edu.co>
2021-09-21 06:49:43 -0500

    Nuevos archivos

---
+++ ProyectoCiclo3.App.Persistencia/Migrations/20210913131234_MigraInicial.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoCiclo3.App.Persistencia.Migrations
+{
+    public partial class MigraInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Encomiendas");
+
+            migrationBuilder.CreateTable(
+                name: "Aviones",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    marca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    modelo = table.Column<int>(type: "int", nullable: false),
+                    numero_asientos = table.Column<int>(type: "int", nullable: false),
+                    numero_banos = table.Column<int>(type: "int", nullable: false),
+                    capacidad_maxima = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aviones", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aviones");
+
+            migrationBuilder.CreateTable(
+                name: "Encomiendas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    peso = table.Column<int>(type: "int", nullable: false),
+                    presentacion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    tipo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Encomiendas", x => x.id);
+                });
+        }
+    }
+}
